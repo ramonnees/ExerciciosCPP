686a0afc2dceaa15570fdc3095e35f97d13c0a22
ramonnees <ramon66625@gmail.com>
2025-10-27 17:46:55 -0300

    Adiciona exercicio 10 - Lista de Tarefas

---
+++ ex10_listaTarefas.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class Tarefa{
+    private:
+    string descricao;
+    bool concluida;
+ 
+    
+    public:
+    Tarefa(string desc){// construtor
+        descricao = desc;
+        concluida = false;
+    }
+
+    void marcarConcluida(){
+        concluida = true;
+    }
+
+    void imprimirTarefa(){
+        cout << (concluida? "[X] " : "[ ] ");
+        cout << descricao << endl;
+
+    }
+};
+
+int main(){
+    cout << "Lista de Tarefas" << endl;
+    vector<Tarefa> listaTarefas;
+    listaTarefas.push_back(Tarefa("Lavar louca")); //adiciona tarefas na lista
+    listaTarefas.push_back(Tarefa("Estudar c++"));
+    listaTarefas.push_back(Tarefa("Fazer compras"));
+
+    listaTarefas[0].marcarConcluida();// marca a primeira tarefa como concluida
+    for(int i=0; i<listaTarefas.size();i++){
+        listaTarefas[i].imprimirTarefa();// imprime todas
+
+    }
+    
+
+}
